3a65085a5de2fafa063f35ab19725e5a5e2b29b5
Clairevitsing <115070408+Clairevitsing@users.noreply.github.com>
2023-12-06 23:38:31 +0100

    sixth commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IUser} from "../interface/IUser.modele";
+import {TokenService} from "../service/token.service";
+import {UserService} from "../service/user.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  users: IUser[] = []
+  user!: IUser;
 
+  constructor(private tokenService: TokenService, private userService: UserService){}
+
+  ngOnInit(): void {
+    this.loadUsersAndFindUserByPseudo();
+  }
+
+  loadUsersAndFindUserByPseudo(): void{
+    this.userService.getAllUsers().subscribe(
+      (users) => {
+        this.users = users['hydra:member'];
+        const pseudo = this.tokenService.getUserPseudo();
+        if (pseudo) {
+          this.user = this.findUserByPseudo(pseudo);
+        }
+      }
+    );
+  }
+
+  findUserByPseudo(pseudo: string): any {
+    return Object.values(this.users).find(user => user.pseudo === pseudo);
+  }
+
+  isLoggedIn(): boolean {
+    return this.tokenService.getIsLogged();
+  }
+
+  logout(): void {
+    this.tokenService.clearTokenAndUserInfos()
+  }
 }
